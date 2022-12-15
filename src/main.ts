cd9c12adbb87399035efef46416c65f20cee96ae
Carolina Villalobos <carito15villalobos@gmail.com>
2022-12-15 17:50:47 -0400

    make it better

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
     {
       cors: true
     });
-  // await app.listen(process.env.PORT || 8080);
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 8080);
+  // await app.listen(3000);
 }
 bootstrap();
